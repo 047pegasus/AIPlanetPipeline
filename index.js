bed57aaad922f62def36061e56bd75ee10c9b8c4
047pegasus <tanishqoct11@gmail.com>
2024-05-14 22:46:42 +0530

    add: initial files & Dockerfile

---
+++ index.js
@@ -0,0 +1,23 @@
+//Write an application to launch a server at port 3000 and return a response with a message "Welcome to AI Planet Canary Release 1.0" when the server is hit with a request on "/" endpoint. Also make sure to add two more endpoints in the application which will return information about the proejct i.e. "The proehct is a simple web server backend application to be deployed for the internship assignment of AI Planet by Tanishq Agarwal on Kubernetees Cluster running ✅" and my contact details including email and author name when the server is hit with a request on "/about" and "/contact" endpoints respectively.
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Welcome to AI Planet Internship Test Web Application Canary Release 1.0");
+});
+
+app.get('/about', (req, res) => {
+    res.send("The project is a simple web server backend application to be deployed for the internship assignment of AI Planet by Tanishq Agarwal on Kubernetees Cluster running ✅");
+});
+
+app.get('/contact', (req, res) => {
+    res.send("Author: Tanishq Agarwal \nEmail: tanishqoct11@gmail.com");
+});
+
+app.listen(7788, () => {
+    console.log("Server started at port 7788");
+});
+
+
